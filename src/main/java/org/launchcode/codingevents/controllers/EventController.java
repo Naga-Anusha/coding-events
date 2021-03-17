1f538eec46a43ce7afa701450cdb48e2c0de30ef
Anusha <anushabt@gmail.com>
2021-03-17 15:36:46 -0500

    my views exercises solution

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -6,19 +6,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
+    private static Map<String, String> events = new HashMap<>();
+
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
+       /* List<String> events = new ArrayList<>();
         events.add("Code With Pride");
         events.add("Strange Loop");
         events.add("Apple WWDC");
         events.add("SpringOne Platform");
+        model.addAttribute("events",events);*/
+
+        events.put("Hour of code", "Getting young minds interested in coding");
+        events.put("StrangeLoop", "Coding conference for professionals sharing ideas");
+        events.put("Apple WWDC", "Apple conference for professionals");
         model.addAttribute("events",events);
         return "events/index";
     }
